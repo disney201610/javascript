670c525d47608815a938173ff260cf43e147f28d
Gael Stol <gaelielmejor@gmail.com>
2025-01-13 14:48:57 -0300

    axios

--- Fetch/script.js
+++ Fetch/script.js
@@ -18,20 +18,24 @@
 
 // solicitarInfoPokemon();
 
-const input = document.querySelector('.value-pokemon');
-const btn = document.querySelector('.btn-pokemon');
-const url = 'https://pokeapi.co/api/v2/pokemon/';
-const pokemon = document.querySelector('.pokemon-propiedades');
+// const input = document.querySelector('.value-pokemon');
+// const btn = document.querySelector('.btn-pokemon');
+// const url = 'https://pokeapi.co/api/v2/pokemon/';
+// const pokemon = document.querySelector('.pokemon-propiedades');
 
 
-btn.addEventListener('click', async function solicitarInfoPokemon() {
-    const url_final = url + input.value.toLowerCase();
-    const res = await fetch(url_final);
-    const data = await res.json();
-    pokemon.innerHTML = '';
-    data.abilities.forEach((ability) => {
-        const pokemoninfo = document.createElement('p');
-        pokemoninfo.textContent = ability.ability.name;
-        pokemon.appendChild(pokemoninfo);
-    });
-});
+// btn.addEventListener('click', async function solicitarInfoPokemon() {
+//     const url_final = url + input.value.toLowerCase();
+//     const res = await fetch(url_final);
+//     const data = await res.json();
+//     pokemon.innerHTML = '';
+//     data.abilities.forEach((ability) => {
+//         const pokemoninfo = document.createElement('p');
+//         pokemoninfo.textContent = ability.ability.name;
+//         pokemon.appendChild(pokemoninfo);
+//     });
+// });
+    
+import axios from 'https://cdn.skypack.dev/axios';
+axios('https://pokeapi.co/api/v2/pokemon/bulbasaur')
+    .then(res => console.log(res.data));
