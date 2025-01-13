daaf061d3f8d61ca651fc17126b8430b1d9306ad
Gael Stol <gaelielmejor@gmail.com>
2025-01-13 11:15:20 -0300

    fetch

---
+++ Fetch/script.js
@@ -0,0 +1,22 @@
+// API
+// Application programming interface
+
+// const URL = 'https://pokeapi.co/api/v2/pokemon/'; // API URL
+// const POKEMON = 'bulbasaur';
+
+// const URL_FINAL = URL + POKEMON;
+
+// console.log(URL_FINAL);
+
+// async function solicitarInfoPokemon() {
+//    const res = await fetch(URL_FINAL);
+//    const data = await res.json();
+//    data.abilities.forEach((ability) => {
+//       console.log(ability.ability.name);
+//    });
+// }
+
+// solicitarInfoPokemon();
+
+const input = document.querySelector('.value-pokemon');
+const btn = document.querySelector('.btn-pokemon');
