db3eca5d8d5ddfb01006a0e090d3d7ae133da28c
Gael Stol <gaelielmejor@gmail.com>
2025-01-13 12:39:25 -0300

    Api de pokemon, hice un trabajo

--- Fetch/script.js
+++ Fetch/script.js
@@ -20,3 +20,18 @@
 
 const input = document.querySelector('.value-pokemon');
 const btn = document.querySelector('.btn-pokemon');
+const url = 'https://pokeapi.co/api/v2/pokemon/';
+const pokemon = document.querySelector('.pokemon-propiedades');
+
+
+btn.addEventListener('click', async function solicitarInfoPokemon() {
+    const url_final = url + input.value.toLowerCase();
+    const res = await fetch(url_final);
+    const data = await res.json();
+    pokemon.innerHTML = '';
+    data.abilities.forEach((ability) => {
+        const pokemoninfo = document.createElement('p');
+        pokemoninfo.textContent = ability.ability.name;
+        pokemon.appendChild(pokemoninfo);
+    });
+});
