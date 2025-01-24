6835cb7602189fb2f9aed096f27e193d7d04cf10
Gael Stol <gaelielmejor@gmail.com>
2025-01-24 16:23:44 -0300

    prototipos

---
+++ prototipos/proto.js
@@ -0,0 +1,9 @@
+// let variable = function () {};
+
+// // __proto__ es un objeto que contiene las propiedades y métodos que son comunes
+// // si queremos accedere a las propiedades de un objeo que creamos nosotros, se tiene que usar prototype
+// console.log(variable.prototype.__proto__);
+
+let variable = ["pedro", 34, null];
+
+console.log(variable);
