92459e5c4666edaa5bf610169c3d4a85effc51b1
Gael Stol <gaelielmejor@gmail.com>
2025-01-27 13:51:39 -0300

    use strict

--- prototipos/proto.js
+++ prototipos/proto.js
@@ -1,9 +1,10 @@
-// let variable = function () {};
+"use strict";
+// use strict es una directiva que se usa para que el código se ejecute en modo estricto
 
 // // __proto__ es un objeto que contiene las propiedades y métodos que son comunes
 // // si queremos accedere a las propiedades de un objeo que creamos nosotros, se tiene que usar prototype
 // console.log(variable.prototype.__proto__);
 
-let variable = ["pedro", 34, null];
+// let variable = ["pedro", 34, null];
 
-console.log(variable);
+// console.log(variable);
