0a432e86797dcb1b7e28f8bea7882eca5f150bad
Gael Stol <gaelielmejor@gmail.com>
2025-02-05 19:25:16 -0300

    agregue comentarios al archivo notifications

--- notification/a.js
+++ notification/a.js
@@ -1,12 +1,15 @@
-'use strict';
+"use strict";
+
+// Verificar si las notificaciones estan disponibles
 
 !(Notification in window)
-? console.log('las notificaciones no estan disponibles')
-: console.log('las notificaciones estan disponibles');
+  ? console.log("las notificaciones no estan disponibles")
+  : console.log("las notificaciones estan disponibles");
 
+// si las notificaciones estan disponibles, new notification
 
-Notification.requestPermission(()=> {
-    if(Notification.permission === 'granted') {
-        new Notification('Hola mundo');
-    }
-})
+Notification.requestPermission(() => {
+  if (Notification.permission === "granted") {
+    new Notification("Hola mundo");
+  }
+});
