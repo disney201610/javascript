e9b4094a396040a68dc27c6e1fbfaeca4bd1388a
Gael Stol <gaelielmejor@gmail.com>
2025-02-05 23:49:18 -0300

    web worker terminado

--- web worker/a.js
+++ web worker/a.js
@@ -3,12 +3,18 @@
 // web worker es para ejecutar código en segundo plano
 
 const worker = new Worker("b.js");
-document.querySelector("button").addEventListener("click",() => ejecutarBucle());
+document
+  .querySelector("button")
+  .addEventListener("click", () => ejecutarBucle());
+worker.addEventListener("message", (e) => {
+  console.log(e.data);
+  worker.terminate();
+});
 
 // el postMessage es para enviar mensajes al worker
 
 const ejecutarBucle = () => {
-  worker.postMessage("todo bien");
+  worker.postMessage("Mensaje 1");
 };
 
 console.log(worker);
--- web worker/b.js
+++ web worker/b.js
@@ -1,5 +1,6 @@
 addEventListener("message", (e) => {
-    if (e.data.length) {
-        
-    }
+  if (e.data.length > 4) {
+    console.log(e.data);
+    postMessage("Mensaje 2");
+  }
 });
