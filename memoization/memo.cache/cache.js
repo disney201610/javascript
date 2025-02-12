250cdb49f40219235fdb0002831ec2c455456ca4
Gael Stol <gaelielmejor@gmail.com>
2025-02-12 15:01:19 -0300

    termine cache, y empeza con serevice worker

--- memoization/memo.cache/cache.js
+++ memoization/memo.cache/cache.js
@@ -1,7 +1,16 @@
 "use strict";
 
 // cache.add sirve para añadir un recurso al cache
+//cache.addALL sirve para añadir varios recursos al cache
+//cache.match sirve para buscar un recurso en el cache
+//cache.matchAll sirve para buscar varios recursos en el cache
+//cache.put sirve para añadir un recurso al cache
+//cache.delete sirve para eliminar un recurso del cache
+//cache.keys sirve para obtener todos los recursos del cache
 
 caches.open("version1").then((cache) => {
   cache.add("index.html");
+  cache.keys().then(keys => {
+    console.log(keys);
+  });
 });
